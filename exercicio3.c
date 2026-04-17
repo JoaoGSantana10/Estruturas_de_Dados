88cda32e3cf5d819bb58ee6cbbec9e2fac1f1977
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-04-17 18:44:31 -0300

    Create exercicio3.c
    ponteiros

---
+++ exercicio3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int numero;
+    numero = 0710;
+
+    printf("O valor do número é: %d", numero);
+    printf("O endereço de memória do número é: %p", &numero);
+    return 0;
+}
