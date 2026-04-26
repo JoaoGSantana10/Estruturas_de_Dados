97522d24a9aff672d20b80a7b81a620a1699714e
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-04-26 13:44:29 -0300

    Update exercicio2.c

--- exercicio2.c
+++ exercicio2.c
@@ -9,8 +9,8 @@ int main () {
     struct pessoa pessoas1 = {"João", 25, 1.80};
     struct pessoa pessoaM = {"João", 26, 1.81};
 
-    printf("Pessoa:  \n nomeP: %s\n idade %d\n altura %f\n", pessoa.nomeP,pessoa.idade, pessoa.altura);
-    printf("PessoaM: \n nomeP: %s\n idade %d\n altura %f\n", pessoaM.nomeP, pessoaM.idade, pessoaM.altura);
+    printf("Pessoa:  \n nomeP: %s\n idade %d\n altura %.2f\n", pessoas1.nomeP,pessoas1.idade, pessoas1.altura);
+    printf("PessoaM: \n nomeP: %s\n idade %d\n altura %.2f\n", pessoaM.nomeP, pessoaM.idade, pessoaM.altura);
     return 0;
 
 }
