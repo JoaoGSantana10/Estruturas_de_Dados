d0615d00c483f8f1e7c201a30253b698df852bed
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-05-14 00:02:35 -0300

    Update exercicio3.c

--- exercicio3.c
+++ exercicio3.c
@@ -4,7 +4,7 @@ int main(){
     int numero;
     numero = 0710;
 
-    printf("O valor do número é: %d", numero);
-    printf("O endereço de memória do número é: %p", &numero);
+    printf("O valor do número é: %d\n", numero);
+    printf("O endereço de memória do número é: %p\n", &numero);
     return 0;
 }
