015e581939bb6d113ca2cc615a9c42d9e4f31d44
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-05-19 15:22:32 -0300

    Create exercicio35.c

---
+++ exercicio35.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Declare um vetor de inteiros de tamanho 10 e inicialize todos
+
+int main(){
+    int v[10] = {0}; // Inicializa todos os elementos com 0
+    int i;
+
+    // como inicializar para contar de 1 a 10?
+
+    for ( i = 0; i < 10; i++){
+        //v[i] = i + 1; // Atribui valores de 1 a 10
+        printf("%d\n", v[i]);
+    }
+    
+
+
+
+    return 0;
+}
