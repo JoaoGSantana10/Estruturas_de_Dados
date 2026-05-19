e4ebdd71b40c61021b5effd0eeeba6627ac981f8
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-05-19 15:22:21 -0300

    Update exercicio33.c

--- exercicio33.c
+++ exercicio33.c
@@ -28,9 +28,48 @@ Pessoa SetPessoa(int idade, float peso, float altura, char nome[50]){
 
 };
 
+/*
+Passagem por referência 
+
+Usar ponteiro para modificar o original
+
+Quando você precisa que a função modifique a struct original, passa um ponteiro com &:
+*/
+
+void SetPessoa(Pessoa *P, int idade, float peso, float altura, char nome[50]){
+    
+    (*P).idade = idade; // forma 1: deresferênciar
+    P->peso = peso; // forma 2: operador seta (mais usada)
+    P->altura = altura;
+    strcpy(P->nome, nome);
+
+    /*
+    Notação com asterisco
+
+    (*P).idade = 15;
+    deresferencia o P, depois acessa o campo Idade
+
+    Notação com seta(preferida)
+
+    P->idade = 15;
+
+    atalho para (*P).idade
+    muito mais legível
+
+    P->campo e (*P).campo são exatamente equivalentes. Use sempre a seta - é o padrão do mercado.
+    
+    */
+
+}
+
+
 int main(){
+    //Pessoa Joao;
+    //Joao = SetPessoa(20, 70.0, 1.75, "João"); // recebe struct
+    //ImprimePessoa(Joao);
+
     Pessoa Joao;
-    Joao = SetPessoa(20, 70.0, 1.75, "João"); // recebe struct
+    SetPessoa(&Joao, 20, 70.0, 1.75, "João"); // passa endereço
     ImprimePessoa(Joao);
     return 0;
 
