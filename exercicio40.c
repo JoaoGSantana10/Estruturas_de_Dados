2699306161a0d5447511605854e30f3ca9bf6fbe
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-05-20 12:40:46 -0300

    Update exercicio40.c

--- exercicio40.c
+++ exercicio40.c
@@ -6,12 +6,13 @@
 int main(){
     FILE *arquivo;
 
-    arquivo = fopen("exemplo.txt", "w"); // Abre o arquivo para escrita (modo "w")
-    fprintf(arquivo, "Olá, este é um exemplo de escrita em arquivo.\n"); // Escreve uma linha no arquivo
-    fprintf(arquivo, "Esta é a segunda linha do arquivo.\n"); // Escreve outra linha no arquivo
+    arquivo = fopen("Produtos.txt", "w"); // Abre o arquivo para escrita (modo "w")
+    fprintf(arquivo, "Produto: Mouse\n  Preço: R$ 50,00\n Quantidade: 10\n"); // Escreve uma linha no arquivo
+    fprintf(arquivo, "\n"); // Escreve uma linha em branco no arquivo
+    fprintf(arquivo, "Produto: Teclado\n  Preço: R$ 100,00\n Quantidade: 5\n"); // Escreve outra linha no arquivo
     fclose(arquivo); // Fecha o arquivo
     
-    arquivo = fopen("exemplo.txt", "r"); // Abre o arquivo para leitura (modo "r")
+    arquivo = fopen("Produtos.txt", "r"); // Abre o arquivo para leitura (modo "r")
     char linha[100]; // Buffer para armazenar cada linha lida do arquivo
     printf("Conteúdo do arquivo:\n");
     while (fgets(linha, sizeof(linha), arquivo)) { // Lê cada linha do arquivo até o final
