87ef7658a268830d08d3fd460dae7eda2bd03917
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-06-11 19:04:12 -0300

    Update exercicio1.c

--- trabalhos-EDA/Trabalhos-Ponteiros/exercicio1.c
+++ trabalhos-EDA/Trabalhos-Ponteiros/exercicio1.c
@@ -3,7 +3,17 @@
 
 int main(){
 
-    
+    int t = 10;
+    float p = 20;
+    int *pt;
+    float *pf;
+    pf = &p;
+    pt = &t;
+
+    printf("O valor que está armazenado no ponteiro  p é: %f\n", *pf);
+    printf("O endereço do ponteiro pf na memória é: %p\n", &pf);
+    printf("O valor que está armazenado no ponteiro t é: %d\n", *pt);
+    printf("O endereço do ponteiro pt na memória é: %p\n", &pt);
     
     return 0;
 }
