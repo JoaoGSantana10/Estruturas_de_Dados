fdd8f1258477ccc27141a2e375c0893ad94a8cb1
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-06-11 15:49:38 -0300

    Update main.c
    adicionando o case do inserir medidas

--- trabalhos-EDA/Trabalho-alocaçao-dinamica/main.c
+++ trabalhos-EDA/Trabalho-alocaçao-dinamica/main.c
@@ -19,7 +19,7 @@ int main(){
             {
                 free(temps.medidas);
             }
-            printf("Quantos dias você deseja armazenar de medidas?");
+            printf("Quantos dias você deseja armazenar de medidas: ");
             scanf("%d", &temps.qtde_dias);
             temps.medidas = (TMedidas*) malloc(temps.qtde_dias * sizeof(TMedidas));
             for(int i; i < temps.qtde_dias; i++){
@@ -28,7 +28,18 @@ int main(){
             temps.qtde_medidas= 0;
             break;
       case 2:
-          printf("Em construção\n");
+            if (temps.medidas == NULL || temps.qtde_medidas >= temps.qtde_dias)
+            {
+                printf("Erro!\n");
+                
+            } 
+            else{
+                printf("Informe o nome da cidade: ");
+                scanf("%s", temps.medidas[temps.qtde_medidas].cidade);
+                printf("Informe a temperatura: ");
+                scanf("%f", temps.medidas[temps.qtde_medidas].temperatura);
+                temps.qtde_medidas ++;
+            }
           break;
       case 3:
           printf("Em construção\n");
