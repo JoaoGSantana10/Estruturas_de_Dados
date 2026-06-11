807773f868054d5dc5a8dadf3da0c1febbb72310
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-06-11 15:37:45 -0300

    Update main.c

---
+++ trabalhos-EDA/Trabalho-alocaçao-dinamica/main.c
@@ -0,0 +1,48 @@
+#include "listas.h"
+
+int main(){
+
+    TTemperaturas temps;
+    int op;
+
+    temps.medidas = NULL;
+
+    do{
+      printf("1 -  Reset\n"); // ler qtde_dias e aloca o vetor
+      printf("2 - Inserir Medida\n"); // ler cidade e temperatura e adicionar no vetor
+      printf("3 - Estatística\n"); // Mostar média, menor e maior temperatura
+      printf("0 - Sair\n"); // Sai do programa
+      scanf("%d", &op);   
+      switch (op){
+      case 1:
+            if (temps.medidas != NULL)
+            {
+                free(temps.medidas);
+            }
+            printf("Quantos dias você deseja armazenar de medidas?");
+            scanf("%d", &temps.qtde_dias);
+            temps.medidas = (TMedidas*) malloc(temps.qtde_dias * sizeof(TMedidas));
+            for(int i; i < temps.qtde_dias; i++){
+                temps.medidas[i].temperatura = (float*) malloc(sizeof(float));
+            }
+            temps.qtde_medidas= 0;
+            break;
+      case 2:
+          printf("Em construção\n");
+          break;
+      case 3:
+          printf("Em construção\n");
+          break;
+      case 0:
+          printf("Encerrando o programa");
+          break;
+      default:
+          break;
+      }
+    } while (op != 0 );
+    
+    return 0;
+}
+
+
+
